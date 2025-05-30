98c01e3d84aaa18226fcde325a916553214b4a5a
ponce08 <yonathanponce2019@gmail.com>
2025-05-30 11:51:28 -0500

    Error found

--- native_modules/dist/fs-module/fs.js
+++ native_modules/dist/fs-module/fs.js
@@ -1,5 +1,10 @@
-import fs from 'fs';
-fs.readFile('example.txt', 'utf8', (err, data) => {
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+fs_1.default.readFile('example.js', 'utf8', (err, data) => {
     if (err) {
         console.log('error al leer el archivo', err);
         return;
