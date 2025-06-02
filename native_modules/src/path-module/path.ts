2e8ab1aa0f0ff1d5290361ec487759884fe2c57b
ponce08 <yonathanponce2019@gmail.com>
2025-06-02 14:05:10 -0500

    path--module

--- native_modules/src/path-module/path.ts
+++ native_modules/src/path-module/path.ts
@@ -1,5 +1,19 @@
-import path from 'path';
+// import path from 'path';
 
-const pathFile = 'native_modules/src/http-module/index.html';
+// const pathFile = 'native_modules/src/http-module/index.html';
 
-console.log(path.basename(pathFile));
+// console.log(path.basename(pathFile));
+// console.log(path.dirname(pathFile));
+// console.log(path.extname(pathFile));
+
+// console.log(path.resolve('src', 'index.html'));
+
+// console.log(path.parse(pathFile));
+// let objPath = {
+//   root: '',
+//   dir: 'native_modules/src/http-module',
+//   base: 'index.html',
+//   ext: '.html',
+//   name: 'index'
+// };
+// console.log(path.format(objPath));
