7f53bbfb1379b369bdfc6f9816b95b7f733f19bf
ponce08 <yonathanponce2019@gmail.com>
2025-06-09 19:06:48 -0500

    typescript-10

--- Practice-typescript/example.ts
+++ Practice-typescript/example.ts
@@ -120,3 +120,23 @@ class Rectangle implements Shape {
 class Square extends Rectangle {
   override hacerSonido() {}
 }
+
+// ?---------------------------------------------------------------
+
+abstract class Polygon {
+  public abstract getArea(): number;
+
+  public toString(): string {
+    return `Polygon[area=${this.getArea()}]`;
+  }
+}
+
+class Rectangles extends Polygon {
+  public constructor(protected readonly width: number, protected readonly height: number) {
+    super();
+  }
+
+  public getArea(): number {
+    return this.width * this.height;
+  }
+}
