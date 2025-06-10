e8d17dfaf6c793990f1be1643aeb11202a6299e6
ponce08 <yonathanponce2019@gmail.com>
2025-06-10 16:15:57 -0500

    typescript-12

--- Practice-typescript/example2.ts
+++ Practice-typescript/example2.ts
@@ -24,3 +24,31 @@ let ordersArrayTS: [Pet, number][] = [
 ];
 
 // ordersArrayTS.push([Pet.Jerboa, 3]);
+
+let petOnSale2 = 'chinchilla';
+let ordersArray2 = [
+  ['rat', 2],
+  ['chinchilla', 1],
+  ['hamster', 2],
+  ['chinchilla', 50]
+];
+
+// Write your code below:
+
+enum Pet2 {
+  Hamster = 'HAMSTER',
+  Rat = 'RAT',
+  Chinchilla = 'CHINCHILLA',
+  Tarantula = 'TARANTULA'
+}
+
+let petOnSaleTS2: Pet = Pet.Chinchilla;
+
+let ordersArrayTS2: [Pet, number][] = [
+  [Pet.Rat, 2],
+  [Pet.Chinchilla, 1],
+  [Pet.Hamster, 2],
+  [Pet.Chinchilla, 50]
+];
+
+// ordersArrayTS.push(['HAMSTER', 1]);
