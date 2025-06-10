183542774f4f069533e5bc5e6be1b4d3770ce976
ponce08 <yonathanponce2019@gmail.com>
2025-06-10 17:05:12 -0500

    typescript-14

--- Practice-typescript/example2.ts
+++ Practice-typescript/example2.ts
@@ -77,3 +77,36 @@ let birthdayBabies: {
 ];
 
 // birthdayBabies.forEach(sayHappyBirthdayWithObject);
+
+// Math Operations
+function add(a: number, b: number): number {
+  return a + b;
+}
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+function divide(a: number, b: number): number {
+  return a / b;
+}
+function wrongAdd(a: number, b: number) {
+  return a + b + '';
+}
+
+// Add your function type below:
+type OperatorFunction = (a: number, b: number) => number;
+
+// Math Tutor Function That Accepts a Callback
+function mathTutor(operationCallback: OperatorFunction) {
+  console.log("Let's learn how to", operationCallback.name, '!');
+  let value25 = operationCallback(2, 5);
+  console.log('When we', operationCallback.name, '2 and 5, we get', value25, '.');
+  console.log('When we', operationCallback.name, value25, 'and 7, we get', operationCallback(value25, 7), '.');
+  console.log('Now fill out this worksheet.');
+}
+
+// Call your functions below:
+// mathTutor(multiply);
+// mathTutor(wrongAdd)
