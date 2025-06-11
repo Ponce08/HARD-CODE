e806608ab4cfa5493498515a4d1249953881fa30
ponce08 <yonathanponce2019@gmail.com>
2025-06-11 14:15:54 -0500

    typescript-18

--- Practice-typescript/example2.ts
+++ Practice-typescript/example2.ts
@@ -190,5 +190,21 @@ function formatValue(value: string | number) {
   }
 }
 
-formatValue('Hiya');
-formatValue(42);
+// formatValue('Hiya');
+// formatValue(42);
+
+function formatListings(listings: (string | number)[]) {
+  return listings.map((listing) => {
+    if (typeof listing === 'string') {
+      return listing.toUpperCase();
+    }
+
+    if (typeof listing === 'number') {
+      return `$${listing.toLocaleString()}`;
+    }
+  });
+}
+
+const result = formatListings(['123 Main St', 226800, '580 Broadway Apt 4a', 337900]);
+
+// console.log(result);
