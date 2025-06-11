0c5eaa7606817de5568a5dfbe20a25bd51a46c30
ponce08 <yonathanponce2019@gmail.com>
2025-06-11 13:58:24 -0500

    typescript-17

--- Practice-typescript/example2.ts
+++ Practice-typescript/example2.ts
@@ -180,3 +180,15 @@ let functionTest = getFilledArray<[number, number]>([3, 4], 6);
 // console.log(functionTest())
 
 // ?______________________________________________
+
+function formatValue(value: string | number) {
+  // Write your code here
+  if (typeof value === 'string') {
+    console.log(value.toLowerCase());
+  } else if (typeof value === 'number') {
+    console.log(value.toFixed(2));
+  }
+}
+
+formatValue('Hiya');
+formatValue(42);
