78e72e87dc087ab7a28b85eada996d6642978029
ponce08 <yonathanponce2019@gmail.com>
2025-06-12 12:19:51 -0500

    typescript-21

--- Practice-typescript/example2.ts
+++ Practice-typescript/example2.ts
@@ -260,3 +260,15 @@ function downloadStatus(status: Status) {
 }
 
 // downloadStatus('downloading');
+
+function formatStatistic(stat: string | number) {
+  if (typeof stat === 'number') {
+    return stat.toFixed(2);
+  }
+  if (typeof stat === 'string') {
+    return stat.toUpperCase();
+  }
+}
+
+// console.log(formatStatistic('Win'));
+// console.log(formatStatistic(0.364));
