c2aa7feccc4ed99e8877bf70b946c50d2bccc250
ponce08 <yonathanponce2019@gmail.com>
2025-06-12 12:35:04 -0500

    typescript-23

--- Practice-typescript/example2.ts
+++ Practice-typescript/example2.ts
@@ -302,4 +302,60 @@ function move(pet: Cat | Fish) {
   }
 }
 
-console.log(move(siameseCat));
+// console.log(move(siameseCat));
+
+type Pasta = {
+  menuName: string;
+  boil: () => string;
+};
+
+type Meat = {
+  menuName: string;
+  panFry: () => string;
+};
+
+const fettuccine = {
+  menuName: 'Fettuccine',
+  boil: () => 'Heat water to 212 degrees'
+};
+
+const steak = {
+  menuName: 'New York Strip Steak',
+  panFry: () => 'Heat oil to 350 degrees'
+};
+
+function prepareEntree(entree: Pasta | Meat) {
+  if ('boil' in entree) {
+    return entree.boil();
+  } else {
+    return entree.panFry();
+  }
+}
+
+// console.log(prepareEntree(fettuccine));
+
+type Metal = {
+  magnetize: () => string;
+};
+
+type Glass = {
+  melt: () => string;
+};
+
+const iron = {
+  magnetize: () => 'Electromagnet activated'
+};
+
+const bottle = {
+  melt: () => 'Furnace set to 2,700 degrees'
+};
+
+function recycle(trash: Metal | Glass) {
+  // Add your code below:
+  if ('magnetize' in trash) {
+    return trash.magnetize();
+  }
+  return trash.melt();
+}
+
+console.log(recycle(iron));
