020af8e173a6151f4b5368127732f8bc672b9722
ponce08 <yonathanponce2019@gmail.com>
2025-06-12 12:36:20 -0500

    typescript-24

--- Practice-typescript/example2.ts
+++ Practice-typescript/example2.ts
@@ -358,4 +358,41 @@ function recycle(trash: Metal | Glass) {
   return trash.melt();
 }
 
-console.log(recycle(iron));
+// console.log(recycle(iron));
+
+// Write an interface here
+interface Run {
+  miles: number;
+}
+
+function updateRunGoal(run: Run) {
+  console.log(`
+Miles left:       ${50 - run.miles}
+Percent of goal:  ${(run.miles / 50) * 100}% complete
+  `);
+}
+
+updateRunGoal({
+  miles: 5
+});
+
+// Write an interface here
+interface Directory {
+  addFile: (name: string) => void;
+}
+
+class DesktopDirectory implements Directory {
+  addFile(name: string) {
+    console.log(`Adding file: ${name}`);
+  }
+
+  showPreview(name: string) {
+    console.log(`Opening preview of file: ${name}`);
+  }
+}
+
+const Desktop = new DesktopDirectory();
+
+Desktop.addFile("lesson-notes.txt");
+Desktop.showPreview("lesson-notes.txt");
+
