86e275a8328d41f73f9f67b769143bde730f60b1
ponce08 <yonathanponce2019@gmail.com>
2025-06-13 15:38:56 -0500

    typescript-27

--- Practice-typescript/example3.ts
+++ Practice-typescript/example3.ts
@@ -14,4 +14,20 @@ function typeUser(obj: TypeEmployee): void {
   console.log(`My name is ${obj.name} and i'm ${obj.age} years old. My sex is ${obj.sex}`);
 }
 
-typeUser({ name: 'Yonathan', age: 35, sex: 'Masculine', working: true });
+// typeUser({ name: 'Yonathan', age: 35, sex: 'Masculine', working: true });
+
+type Admin = {
+  rol: 'Admin';
+  permisos: string[];
+};
+
+type Cliente = {
+  rol: 'Cliente';
+  puntos: number;
+};
+
+function esAdmin(usuario: Admin | Cliente) {
+  return usuario.rol === 'Admin';
+}
+
+const u: Admin | Cliente = { rol: 'Cliente', puntos: 5 };
