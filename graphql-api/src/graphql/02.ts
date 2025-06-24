784377d38bc212629594073c5fa2bdaee25339b9
ponce08 <yonathanponce2019@gmail.com>
2025-06-24 16:27:48 -0500

    24/06/2025

---
+++ graphql-api/src/graphql/02.ts
@@ -0,0 +1,27 @@
+import { buildSchema } from 'graphql';
+
+const schema = buildSchema(/* GraphQL */ `
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
+  input UserInput {
+    name: String!
+    email: String!
+  }
+
+  type Query {
+    getUsers: [User]!
+    getUser(id: ID!): User
+  }
+
+  type Mutation {
+    createUser(input: UserInput!): User!
+    updateUser(id: ID!, input: UserInput!): User!
+    deleteUser(id: ID!): String!
+  }
+`);
+
+export default schema;
