92c811c65469b867e0ceccb5fbde6b372168670c
ponce08 <yonathanponce2019@gmail.com>
2025-07-02 17:21:32 -0500

    typescript-methods-02

--- typescript-methods/methods-01.ts
+++ typescript-methods/methods-01.ts
@@ -1,8 +1,20 @@
-// git add . && git commit -m 'typescript-methods-01' && git push
+// git add . && git commit -m 'typescript-methods-02' && git push
 
 // 1. map() – Transforma cada elemento
 const numeros = [1, 2, 3];
 const alCuadrado = numeros.map(n => n * n);
 console.log(alCuadrado); // [1, 4, 9]
 
+// 2. filter() – Filtra elementos que cumplan una condición
+const edades = [12, 18, 25, 30];
+const mayoresDeEdad = edades.filter(e => e >= 18);
+console.log(mayoresDeEdad); // [18, 25, 30]
+
+// 3. find() – Busca el primer elemento que cumpla una condición
+const usuarios = [
+  { id: 1, nombre: "Ana" },
+  { id: 2, nombre: "Luis" }
+];
+const usuario = usuarios.find(u => u.id === 2);
+console.log(usuario); // { id: 2, nombre: "Luis" }
 
