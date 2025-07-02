18ccbeca3a6a51029a7f886fe73580025034302e
ponce08 <yonathanponce2019@gmail.com>
2025-07-02 17:20:21 -0500

    typescript-methods-01

---
+++ typescript-methods/methods-01.ts
@@ -0,0 +1,8 @@
+// git add . && git commit -m 'typescript-methods-01' && git push
+
+// 1. map() – Transforma cada elemento
+const numeros = [1, 2, 3];
+const alCuadrado = numeros.map(n => n * n);
+console.log(alCuadrado); // [1, 4, 9]
+
+
