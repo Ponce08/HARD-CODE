3102739ba608d318c643fdd72929931ba880ec65
ponce08 <yonathanponce2019@gmail.com>
2025-07-04 20:08:40 -0500

    typescript-example5-03

--- Practice-typescript/example5.ts
+++ Practice-typescript/example5.ts
@@ -12,3 +12,16 @@ function filtrarPares(numeros: number[]): number[] {
 // Prueba
 console.log(filtrarPares([1, 2, 3, 4, 5, 6])); // [2, 4, 6]
 // ?------------------------------------------------------------------
+class Persona1 {
+  constructor(public nombre: string, public edad: number) {}
+
+  saludar(): string {
+    return `Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años.`;
+  }
+}
+
+// Prueba
+const persona_ = new Persona1("Juan", 30);
+console.log(persona1.saludar());
+// Hola, mi nombre es Juan y tengo 30 años.
+// ?------------------------------------------------------------------
