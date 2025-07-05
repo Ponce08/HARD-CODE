676c94cc0b4c0dc5af74fa1a1d7797d140154a81
ponce08 <yonathanponce2019@gmail.com>
2025-07-04 20:07:24 -0500

    typescript-example5-02

--- Practice-typescript/example5.ts
+++ Practice-typescript/example5.ts
@@ -5,3 +5,10 @@ function sumar(a: number, b: number): number {
 // Prueba
 console.log(sumar(5, 3)); // 8
 // ?------------------------------------------------------------------
+function filtrarPares(numeros: number[]): number[] {
+  return numeros.filter((n) => n % 2 === 0);
+}
+
+// Prueba
+console.log(filtrarPares([1, 2, 3, 4, 5, 6])); // [2, 4, 6]
+// ?------------------------------------------------------------------
