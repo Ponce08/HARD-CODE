837c2aec9eef356b2fc2d8411d094aebdb1268c3
ponce08 <yonathanponce2019@gmail.com>
2025-07-04 20:06:10 -0500

    typescript-example5-01

---
+++ Practice-typescript/example5.ts
@@ -0,0 +1,7 @@
+function sumar(a: number, b: number): number {
+  return a + b;
+}
+
+// Prueba
+console.log(sumar(5, 3)); // 8
+// ?------------------------------------------------------------------
