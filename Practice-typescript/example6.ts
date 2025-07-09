c9953cb2feefe779daa340ec62fc1d2b3e3cf508
ponce08 <yonathanponce2019@gmail.com>
2025-07-08 20:52:04 -0500

    typescript-example6-02

--- Practice-typescript/example6.ts
+++ Practice-typescript/example6.ts
@@ -1,3 +1,5 @@
+// ! git add . && git commit -m 'typescript-example6-02' && git push
+
 // 🟡 S — Single Responsibility Principle (SRP)
 // "Una clase debe tener una única responsabilidad o razón para cambiar."
 class Invoice {
@@ -13,3 +15,34 @@ class InvoicePrinter {
     console.log(`Total: ${invoice.calculateTotal()}`);
   }
 }
+// 👉 Aquí Invoice solo calcula y InvoicePrinter imprime. Cada uno tiene su única responsabilidad.
+// *------------------------------------------------------------
+// 🔵 O — Open/Closed Principle (OCP)
+// "Las clases deben estar abiertas para extensión, pero cerradas para modificación."
+interface Discount {
+  apply(price: number): number;
+}
+
+class NoDiscount implements Discount {
+  apply(price: number): number {
+    return price;
+  }
+}
+
+class PercentageDiscount implements Discount {
+  constructor(private percentage: number) {}
+
+  apply(price: number): number {
+    return price - price * (this.percentage / 100);
+  }
+}
+
+class PriceCalculator {
+  constructor(private discount: Discount) {}
+
+  finalPrice(price: number): number {
+    return this.discount.apply(price);
+  }
+}
+// 👉 Puedes agregar nuevos descuentos sin modificar PriceCalculator.
+// *--------------------------------------------------------------------
